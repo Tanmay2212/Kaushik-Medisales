dc5af70ea1ba69c609093941ae655e6a607fff2e
Tanmay Sharma <tanmaysharma2000k@gmail.com>
2026-06-12 00:08:54 +0530

    Chunk 5: Create app configuration

---
+++ backend/apps/alternate_medicine/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AlternateMedicineConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.alternate_medicine'
+    verbose_name = 'Alternate Medicine Management'
